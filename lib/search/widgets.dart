b56664863220e1204f529643c7afcf99d69563e0
LMuchapirei <muchapireil@gmail.com>
2024-12-30 22:15:41 +0200

    adjustments to the artist card for the card view

--- lib/search/widgets.dart
+++ lib/search/widgets.dart
@@ -31,23 +31,59 @@ class AlbumCard extends StatelessWidget {
           SizedBox(height: 8.h),
           Text(
             title,
-            style: TextStyle(color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold),
+            style: TextStyle(color: Colors.white, fontSize: 16.sp, fontWeight: FontWeight.bold),
             maxLines: 1,
             overflow: TextOverflow.ellipsis,
           ),
          Text(
-          '$artists ',
-            style: TextStyle(color: Colors.grey, fontSize: 14),
+          artists,
+            style: TextStyle(color: Colors.grey, fontSize: 14.sp),
             maxLines: 1,
             overflow: TextOverflow.ellipsis,
           ),
           Text(
-            '$year',
-            style: TextStyle(color: Colors.grey, fontSize: 14),
+            year,
+            style: TextStyle(color: Colors.grey, fontSize: 14.sp),
             maxLines: 1,
             overflow: TextOverflow.ellipsis,
           ),
         ],
+    );
+  }
+}
+
+class ArtistCard extends StatelessWidget {
+  final String imageUrl;
+  final String name;
+
+  const ArtistCard({super.key, 
+    required this.imageUrl,
+    required this.name,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        // Artist Image
+        ClipRRect(
+          borderRadius: BorderRadius.circular(80.h),
+          child: Image.network(
+            imageUrl,
+            height: 40.h,
+            width:40.h,
+            fit: BoxFit.cover,
+          ),
+        ),
+        SizedBox(width: 8.h),
+        Text(
+          name,
+          style: TextStyle(color: Colors.white, fontSize: 12.sp, fontWeight: FontWeight.bold),
+          maxLines: 1,
+          overflow: TextOverflow.ellipsis,
+        ),
+      ],
     );
   }
 }
